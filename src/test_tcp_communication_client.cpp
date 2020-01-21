ed07cb35524b68047b0c13db2cdbe355ffeaa8e3
Alexandre HUBERT <alexandre_hubert@hotmail.fr>
2020-01-22 00:57:15 +0100

    Add client init with server port and address + Enable the client to request message ID to server

--- src/test_tcp_communication_client.cpp
+++ src/test_tcp_communication_client.cpp
@@ -21,9 +21,31 @@ int main()
 {
 	cout << "Test C++ library for TCP communication : Client" << endl;
 
-	CTcpClient l_CTcpServer;
-	l_CTcpServer.initTcpClient();
-	l_CTcpServer.startTcpClient();
+	string 	l_clientInputMsg;
+	int 	l_clientSocket;
+	int 	l_clientRequestedMsgId;
+
+	CTcpClient l_CTcpClient(54000, "127.0.0.1");
+	l_clientSocket = l_CTcpClient.initTcpClient();
+	if(l_clientSocket != -1)
+	{
+		//	While loop: send and receive message back from server
+			do
+			{
+				// Enter msg id
+					cout << "> Client requested msg id : ";
+					getline(cin, l_clientInputMsg);
+					l_clientRequestedMsgId = strtoul(l_clientInputMsg.c_str(), NULL, 16);
+
+				// Send requested message ID to server
+					l_CTcpClient.sendRequestedMsgIdToServer(l_clientRequestedMsgId);
+			} while(true);
+	}
+	else
+	{
+		cerr << "TCP client is not initialized! Quitting" << endl;
+		return -1;
+	}
 
     return 0;
 }
