f7866cde1e97f068c7bb53a7fefe97ceb2f32293
Alexandre HUBERT <alexandre_hubert@hotmail.fr>
2020-01-27 22:09:07 +0100

    Change test according to server and client evolutions

--- src/test_tcp_communication_client.cpp
+++ src/test_tcp_communication_client.cpp
@@ -1,19 +1,16 @@
 /*
- * test_tcp_communication_client.cpp
- *
- *  Created on: 13 janv. 2020
- *      Author: ahu
+ * "test_tcp_communication_client.cpp"
  */
 
 
 
 #include <iostream>
-#include <COM_TcpClient.hpp>
+#include <TCP_Client.hpp>
 
 
 
 using namespace std;
-using namespace COM;
+using namespace TCP;
 
 
 
@@ -21,9 +18,16 @@ int main()
 {
 	cout << "Test C++ library for TCP communication : Client" << endl;
 
-	string 	l_clientInputMsg;
-	int 	l_clientSocket;
-	int 	l_clientRequestedMsgId;
+	SPathMsgBody 			l_pathMsgBody;
+	SPathCorrectionMsgBody 	l_pathCorrectionMsgBody;
+	SWorkShopOrderMsgBody	l_workShopOrderMsgBody;
+	SStopMsgBody		 	l_stopMsgBody;
+	SWorkShopReportMsgBody	l_workShopReportMsgBody;
+	SBitReportMsgBody		l_bitReportMsgBody;
+	SErrorMsgBody 			l_errorMsgBody;
+	string 					l_clientInputMsg;
+	int 					l_clientSocket;
+	int 					l_clientRequestedMsgId;
 
 	CTcpClient l_CTcpClient(54000, "127.0.0.1");
 	l_clientSocket = l_CTcpClient.initTcpClient();
@@ -37,8 +41,47 @@ int main()
 					getline(cin, l_clientInputMsg);
 					l_clientRequestedMsgId = strtoul(l_clientInputMsg.c_str(), NULL, 16);
 
-				// Send requested message ID to server
-					l_CTcpClient.sendRequestedMsgIdToServer(l_clientRequestedMsgId);
+				// Request message to server
+					switch(l_clientRequestedMsgId)
+					{
+						case MSG_ID_PATH:
+							cout << "> Requested message to server : MSG_ID_PATH\n";
+							l_CTcpClient.requestdMsgToServer(l_clientRequestedMsgId, &l_pathMsgBody);
+							break;
+
+						case MSG_ID_PATH_CORRECTION:
+							cout << "> Requested message to server : MSG_ID_PATH_CORRECTION\n";
+							l_CTcpClient.requestdMsgToServer(l_clientRequestedMsgId, &l_pathCorrectionMsgBody);
+							break;
+
+						case MSG_ID_WORKSHOP_ORDER:
+							cout << "> Requested message to server : MSG_ID_WORKSHOP_ORDER\n";
+							l_CTcpClient.requestdMsgToServer(l_clientRequestedMsgId, &l_workShopOrderMsgBody);
+							break;
+
+						case MSG_ID_STOP:
+							cout << "> Requested message to server : MSG_ID_STOP\n";
+							l_CTcpClient.requestdMsgToServer(l_clientRequestedMsgId, &l_stopMsgBody);
+							break;
+
+						case MSG_ID_WORKSHOP_REPORT:
+							cout << "> Requested message to server : MSG_ID_WORKSHOP_REPORT\n";
+							l_CTcpClient.requestdMsgToServer(l_clientRequestedMsgId, &l_workShopReportMsgBody);
+							break;
+
+						case MSG_ID_BIT_REPORT:
+							cout << "> Requested message to server : MSG_ID_BIT_REPORT\n";
+							l_CTcpClient.requestdMsgToServer(l_clientRequestedMsgId, &l_bitReportMsgBody);
+							break;
+
+						case MSG_ID_ERROR:
+							cout << "> Requested message to server : MSG_ID_ERROR\n";
+							l_CTcpClient.requestdMsgToServer(l_clientRequestedMsgId, &l_errorMsgBody);
+							break;
+
+						default:
+							cout << "> Unknown message ID\n";
+					}
 			} while(true);
 	}
 	else
@@ -49,4 +92,3 @@ int main()
 
     return 0;
 }
-
